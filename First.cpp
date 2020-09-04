af15ec974cf6986cc361d253ca6c158e8bfa3eeb
vanikiopik <vanikiopik@gmail.com>
2020-09-04 21:08:49 +0300

    LABA 1.1

--- First.cpp
+++ First.cpp
@@ -8,17 +8,19 @@ int main()
 {
     double a, result_one, result_two;
     cout << "Emput a number - ";
+    //Checking input for numbers
     while (!(cin >> a)) 
     {
         cout << "Wrong input!";
         return 0;
     }
-    //-----
+    //Calculating for 1st result
     result_one = cos(a) + sin(a) +
         cos(3 * a) + sin(3 * a);
-    //-----
+    //Calculating for 2nd result
     result_two = 2 * sqrt(2) * cos(a) * sin(M_PI / 4 + 2 * a);
-    //-----
+
+    //Check difference between 1st and 2nd result
     if (result_one = result_two) 
     {
         cout << result_one << "=" << result_two << endl;
